2c6435e42424b13988e099948b55d040e38ff898
Onur Ozay <oonurozay@gmail.com>
2017-07-05 16:56:22 -0700

    Add views and templates

--- mystore/shop/models.py
+++ mystore/shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
@@ -12,9 +13,6 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
-    def get_absolute_url(self):
-        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -36,6 +34,4 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('shop:product_detail', args=[self.id, self.slug])
 
