cf7e9f2b0542c02917ec06da08240251b8872dd1
Onur Ozay <oonurozay@gmail.com>
2017-07-06 13:13:42 -0700

    Add Shopping Cart and context processor

--- mystore/mystore/urls.py
+++ mystore/mystore/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^',include('shop.urls', namespace='shop'))
+    url(r'^cart/', include('cart.urls', namespace='cart')),
+    url(r'^',include('shop.urls', namespace='shop')),
+
 ]
 
 
