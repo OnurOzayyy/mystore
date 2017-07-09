0239e713e17ee49ee79db422d5e44ffd4b5a369e
Onur Ozay <oonurozay@gmail.com>
2017-07-08 23:03:38 -0700

    Add orders and Celery

---
+++ mystore/mystore/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+#set the default django settings module for the celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mystore.settings')
+
+app = Celery('mystore')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
